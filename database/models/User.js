88fb34cde5186583839910d7d7c14501e045323b
antonella <3lmbmartinez@gmail.com>
2023-11-03 16:53:13 -0300

    database y models, sequelize

---
+++ database/models/User.js
@@ -0,0 +1,60 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = 'User'; 
+    let cols = {
+        id: {
+            type: dataTypes.BIGINT.UNSIGNED,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+
+        usr_email: {
+            type: dataTypes.STRING(100),
+            allowNull: false
+        },
+
+        usr_password: {
+            type: dataTypes.STRING(100),
+            allowNull: false
+        },
+        
+        usr_name: {
+            type: dataTypes.STRING(100),
+            allowNull: false
+        },
+       
+        usr_address: {
+            type: dataTypes.STRING(255),
+            allowNull: true
+        },
+
+        tax_id: {
+            type: dataTypes.INTEGER.UNSIGNED,
+            allowNull: true
+        },
+
+        usr_tax_number: {
+            type: dataTypes.BIGINT.UNSIGNED,
+            allowNull: true
+        },
+
+        category_id: {
+            type: dataTypes.BIGINT.UNSIGNED,
+            allowNull: false
+        }
+
+    };
+
+    let config = {
+        timestamps: true,
+        freezeTableName: true,
+        tableName: 'users',
+        paranoid: true,
+        createdAt: 'created_at',
+        updatedAt: 'updated_at',
+        deletedAt: 'deleted_at'
+    }
+    const User = sequelize.define(alias,cols,config);
+    return User;
+    }
+
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,21 @@
+const db = require('../../database/models')
+const sequelize = db.sequelize;
+
+const Users = db.User;
+
 module.exports = {
     login: (req, res) => {
         return res.render('login')
     },
 
-    loginProcess: (req, res) => {
+    loginProcess:async(req, res) => {
+        try {
+            const usuarios = await Users.findAll()
+           console.log(usuarios); 
+        } catch (error) {
+           console.log(error) 
+        }
+
         console.log("Ingresaste Datos Login ")
         console.log("Minuto 48 clase 32")
 
