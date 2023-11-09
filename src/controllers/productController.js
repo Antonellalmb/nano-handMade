44df2fdfdeab75ad54c81e062cac563ba5948d8d
Gabriel Betelu <gabrielbetelu@yahoo.com.ar>
2023-11-08 22:49:34 -0300

    agrego por post creación de registros de Collections y de Discounts

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,7 +1,68 @@
+const db = require('../database/models');
+const sequelize = db.sequelize;
+const { Op } = require('sequelize');
+//const { validationResult } = require("express-validator");
+
+const Collections = db.Collection;
+const Discounts = db.Discount;
+
 module.exports = {
+// *********************************    
+// Product's Table  Controllers
+// *********************************  
     products : (req, res) => {
         return res.render('./products/products')
-    }
+    },
+    
+
+// *********************************    
+// Collection's Table  Controllers
+// *********************************    
+    collections : (req, res) => {
+        return res.render('./products/collectionsTable')
+    },
+
+    processCollections :  async (req, res) => {
+        console.log("Entraste por post a processCollections");
+        try {
+            await Collections.create({
+                name: req.body.collectionName,
+                description: req.body.detail,
+                discount_id: null
+            })
+            
+        } catch (error) {
+            console.log(error);
+        }
+    
+
+        return res.redirect('/product/collectionsTable')
+    },
+
+
+// *********************************    
+// Discount's Table  Controllers
+// *********************************    
+    discounts : (req, res) => {
+        return res.render('./products/discountsTable')
+    },
+
+    processDiscounts :  async (req, res) => {
+        console.log("Entraste por post a processDiscounts");
+        try {
+            await Discounts.create({
+                discount_code: req.body.discountName,
+                discount: req.body.discountPercentage
+            })
+            
+        } catch (error) {
+            console.log(error);
+        }
+    
+
+        return res.redirect('/product/discountsTable')
+    },
+
 
 
     
