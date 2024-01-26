77adcc88785cc9edcc90668734b2f117fa5706b2
Gabriel Betelu <gabrielbetelu@yahoo.com.ar>
2024-01-26 17:55:18 -0300

    Integro MercadoPago checkout

--- assets/js/chartDiv.js
+++ assets/js/chartDiv.js
@@ -11,13 +11,23 @@ function ready() {
     }
     let chart = JSON.parse(localStorage.getItem('chart'));
     // Ya leido el contenido del carrito, uso la función showChart para mostrar el contenido dinámicamente
-    showChart(chart)
-    
+    showChart(chart);
+
+/*    let orderData = document.getElementById('orderDataInfo')
+    let checkoutBtn = document.getElementById('checkoutBtn')
+    checkoutBtn.addEventListener("click", async () => {
+        console.log(orderData.value)
+    })
+*/    
 }
 
 function showChart(chart) {
     let divChart = document.getElementById('chartItem');
     let divSummary = document.getElementById('chartSummary');
+
+    // creo variable orderData que vamos a usar en MercadoPago
+    let orderData = [];
+
     divChart.innerHTML = ``;
     divSummary.innerHTML = ``;
     console.log(chart);
@@ -53,28 +63,142 @@ function showChart(chart) {
                     <p>Subtotal R$ ${product.productPrice * product.productQuantity}</p>
                 </div>
             </div>
+            
             `
+
+            // En orderData vamos acumulando los objetos que tenemos en el carrito con la info
+            // que necesitamos pasarle en las preferencias a MercadoPago
+            orderData.push({
+                title: product.productName,
+                unit_price: product.productPrice,
+                quantity: product.productQuantity
+            })
+
             totalChart += product.productPrice * product.productQuantity
             totalQuantity += product.productQuantity
         })
+        console.log(orderData)
         console.log(totalQuantity)
         console.log(totalChart)
         divSummary.innerHTML += `
         <p>Total de Itens: &nbsp &nbsp &nbsp &nbsp &nbsp ${totalQuantity} </p>
         <p>Subtotal: &nbsp &nbsp &nbsp $R ${totalChart} </p>
         <p class="commentSummary">O valor de entrega vai ser calculado nas próximas etapas</p>
-        <div class="continueButton">
+        <div id="checkoutBtn" class="continueButton">
             <button class="buttonContinue">Continuar com a compra</button>
         </div>
-        <div class="emptyChart">
+        <div id="wallet_container"></div>
+        <div class="emptyChart" id="divEmptyChart">
             <button class="buttonEmpty" onClick=emptyChart() ><i class="fa-solid fa-face-frown"></i> &nbsp &nbspEsvaziar à sacola &nbsp &nbsp<i class="fa-solid fa-trash"></i></button>
         </div>
+        
 
         `
         
     }
     
+    //MERCADOPAGO //////////////////////////////////////////////////////////////////////////////
+    // Add SDK credentials
+    // REPLACE WITH YOUR PUBLIC KEY AVAILABLE IN: https://developers.mercadopago.com/panel
+    const mp = new MercadoPago('TEST-26191e0f-be78-4122-aef8-b49b17ff26d6', {
+        locale: "es-AR" // Los más comunes son: 'pt-BR', 'es-AR' and 'en-US'
+    });
+
+    // Capturo el div del botón "Continuar com a compra" para que al hacer "click"
+    // elimine el botón de "Vaciar el carrito" y se ejecute el proceso de crear la preferencia 
+    // de MP y luego se agregue el botón de MercadoPago para pagar
+    let checkoutBtn = document.getElementById('checkoutBtn')
+    checkoutBtn.addEventListener("click", async () => {
+    //    console.log(orderData)
+        try {
+            // Elimina botón de "vaciar carrito" y el de "Continuar con la compra"
+            document.getElementById('divEmptyChart').innerHTML = ``
+            checkoutBtn.innerHTML = ``
+            document.getElementById('wallet_container').removeAttribute('class');
+            // enviamos por POST la info de los artículos del carrito a create_preference
+            // que guardamos en orderData
+            console.log("Antes del fetch de la ruta /create_preference")
+            console.log(JSON.stringify(orderData))
+            const response = await fetch ("/create_preference" , {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },                
+                body: JSON.stringify(orderData),
+            });
+            
+            // Acá recibimos la respuesta que nos devuelve MP con el id de MP para el proceso de pago
+            const preference = await response.json()
+
+            // Agregamos el botón de pago de MP vinculado al id de preference
+            createCheckoutButton(preference.id)
+            
+        } catch (error) {
+            console.log(error)
+        }
+       
+    });
+
+    
+    
 }
+
+// función que crea el botón con el id de la preferencia creado por MP
+function createCheckoutButton(preferenceId) {
+    //MERCADOPAGO //////////////////////////////////////////////////////////////////////////////
+    // Add SDK credentials
+    // REPLACE WITH YOUR PUBLIC KEY AVAILABLE IN: https://developers.mercadopago.com/panel
+    const mp = new MercadoPago('TEST-26191e0f-be78-4122-aef8-b49b17ff26d6', {
+        locale: "es-AR" // Los más comunes son: 'pt-BR', 'es-AR' and 'en-US'
+    });
+    // los bricks son creados por MP para crear el botón de pago
+    const bricksBuilder = mp.bricks();
+
+    // creamos esta función asíncrona que va a renderizar el botón de pago con el id de MP
+    const renderComponent = async () => {
+        // La siguiente línea es para que en caso de que el usuario haga más de un click en el botón de "Continuar com a compra"
+        // no se genere otro botón de pago de MercadoPago y el método unmount() es propio de MP
+        // ******  Esto no funcionó por eso puse la línea de código  "checkoutBtn.innerHTML = ``" para evitar el doble click
+        // ------------->>>     if (window.checkoutButton) window.checkoutButton.unmount();
+
+
+        await bricksBuilder.create("wallet", "wallet_container", {
+            initialization: {
+                preferenceId: preferenceId,
+            },
+            customization: {
+                texts: {
+                    action: 'pay',
+                    valueProp: 'security_details',
+                },
+                visual: {
+                    buttonBackground: 'default',
+                    borderRadius: '6px',
+                    valuePropColor: 'white',
+                    verticalPadding: '8px',
+                    horizontalPadding:'0px',
+                    buttonHeight: '48px'
+                },
+                checkout: {
+                    theme: {
+                        elementsColor: "#4287F5",
+                        headerColor: "#4287F5",
+                    },
+                },
+           },
+            
+         });
+    };
+
+    renderComponent()
+
+}
+// FINAL MERCADOPAGO ///////////////////////////////////////////////////////////////////////////
+
+
+
+
+
 
 function emptyChart() {
     let chart = [];
